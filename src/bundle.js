6904a2718fa63c6ebc72987d9b57617c7a7ce21b
madasliver <adam.epi.lefty@gmail.com>
2026-06-16 14:32:30 +0200

    style: increase icon cell size for more breathing room on desktop
    All cell sizes +10px (s:78 m:90 l:106 xl:124). Icon sizes unchanged,
    so icons gain ~10px of clear space above/below in the grid.

--- src/bundle.js
+++ src/bundle.js
@@ -81,10 +81,10 @@ var FOLDER_COLORS = [
 
 // src/modules/sizes.ts
 var SIZES = {
-  s: { icon: "54px", img: "26px", label: "6px", search: "8px", pad: "11px", word: "22px", sw: "460px", cell: "68px" },
-  m: { icon: "66px", img: "32px", label: "7px", search: "9px", pad: "13px", word: "28px", sw: "540px", cell: "80px" },
-  l: { icon: "80px", img: "40px", label: "8px", search: "10px", pad: "15px", word: "34px", sw: "600px", cell: "96px" },
-  xl: { icon: "96px", img: "50px", label: "9px", search: "11px", pad: "18px", word: "40px", sw: "660px", cell: "114px" }
+  s: { icon: "54px", img: "26px", label: "6px", search: "8px", pad: "11px", word: "22px", sw: "460px", cell: "78px" },
+  m: { icon: "66px", img: "32px", label: "7px", search: "9px", pad: "13px", word: "28px", sw: "540px", cell: "90px" },
+  l: { icon: "80px", img: "40px", label: "8px", search: "10px", pad: "15px", word: "34px", sw: "600px", cell: "106px" },
+  xl: { icon: "96px", img: "50px", label: "9px", search: "11px", pad: "18px", word: "40px", sw: "660px", cell: "124px" }
 };
 
 // src/modules/folder.ts
